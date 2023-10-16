075ed4f87f201b2cd1b7f8621954c7377a708f00
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2023-10-16 11:13:00 +0200

    add dependency injection and change accepted origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,8 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234',
+  'http://127.0.0.1:8080',
+  'http://127.0.0.1:1234',
   'https://movies.com',
   'https://midu.dev'
 ]
